09af8e3c4a6904a2197d7fe318a6b639e4f61fcf
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-12 13:55:07 -0400

    commit tree b2259d33c41d7e49adc7433e233d6500c839ac38

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+validates :title, presence: true
+validates :content, length: {minimum: 250}
+validates :summary, length: {maximum: 250}
+validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+validate  :wont_believe
+
+def wont_believe
+    unless title && title.match?(/Won't Believe/)
+        errors.add(:title, "Must include 'Won't Believe' in the title")
+    end
+end
 end
